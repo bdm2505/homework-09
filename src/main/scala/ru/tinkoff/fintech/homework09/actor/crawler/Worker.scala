d1a200285b84a79ee05c306081973db4db3e02ee
bdm <bdm1999@yandex.ru>
2018-11-30 00:59:00 +0500

    поправил akka Worker

--- src/main/scala/ru/tinkoff/fintech/homework09/actor/crawler/Worker.scala
+++ src/main/scala/ru/tinkoff/fintech/homework09/actor/crawler/Worker.scala
@@ -1,20 +1,33 @@
 package ru.tinkoff.fintech.homework09.actor.crawler
+
 import akka.actor.{Actor, ActorLogging, ActorRef}
 
 class Worker(http: Http, parser: Parsr, master: ActorRef) extends Actor with ActorLogging {
   implicit val ec = context.dispatcher
+
+  var urls: List[Url] = List.empty
   var free = true
 
+  def work(): Unit = if (free && urls.nonEmpty) {
+    free = false
+    val url = urls.head
+    urls = urls.tail
+    http.get(url)
+      .map(parser.links)
+      .recover { case _: Exception => Nil }
+      .foreach(self ! Result(url, _))
+  }
+
+
   override def receive: Receive = {
-    case Crawl(url) if free  =>
-      free = false
-      http.get(url).map(parser.links).recover{
-        case _:Exception => Nil
-      }.foreach(self ! Result(url, _))
+    case Crawl(url) =>
+      urls ::= url
+      work()
 
     case Result(url, links) =>
-      free = true
       master ! CrawlResult(url, links)
+      free = true
+      work()
   }
 
 }
